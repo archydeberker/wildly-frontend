89ec0e0ecf0ad4e358ad522785c46d37a0246008
Archy de Berler <archy.deberker@gmail.com>
2019-12-01 13:54:09 -0500

    Add running loading icon

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import CircularProgress from '@material-ui/core/CircularProgress';
+import React from "react";
+import running from "../assets/icons/running.gif";
+import { Typography } from "@material-ui/core";
 
 export default function Loading() {
-    return (
-    <div height='100vh' style={{verticalAlign:'middle', display:'block',
-                                                      position: 'absolute', top: '50%', left: '50%'}}>
-                                                        <CircularProgress color='primary'/></div>)
+  return (
+    <div
+      height="100vh"
+      style={{
+        verticalAlign: "middle",
+        display: "block",
+        position: "absolute",
+        top: "47%",
+        left: "47%"
+      }}
+    >
+      <img
+        src={running}
+        style={{ opacity: 0.5, width: "4rem", height: "4rem" }}
+      />
+      <Typography style={{ opacity: 0.5 }}>Loading...</Typography>
+    </div>
+  );
 }
