6897eb07be422339f2b79fc80c9e5d35496e8bf9
Archy de Berler <archy.deberker@gmail.com>
2019-12-01 09:48:53 -0500

    Add fishing

--- src/data/activities.js
+++ src/data/activities.js
@@ -1,2 +1,2 @@
- const default_activities = ['climb', 'run', 'hike', 'paddle', 'camp', 'ski', 'backcountry ski', 'skate', 'snowshoe', 'horseride', 'swim', 'dive']
+ const default_activities = ['climb', 'run', 'hike', 'paddle', 'camp', 'ski', 'backcountry ski', 'skate', 'snowshoe', 'horseride', 'swim', 'dive', 'fish']
  export default  default_activities.map(input => ({label:input, value: input}))
