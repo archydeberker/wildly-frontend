25b77b3020e13bb8766f89c12c919ce291630279
Archy de Berler <archy.deberker@gmail.com>
2019-12-18 10:54:21 +0000

    WIP on converting discover view to horizontal list

--- src/pages/DiscoverMap.js
+++ src/pages/DiscoverMap.js
@@ -1,91 +1,17 @@
 import React, { useState } from "react"
 import MapView from "../components/google-maps/MapView"
 import Grid from "@material-ui/core/Grid"
-import Card from "@material-ui/core/Card"
-import CardContent from "@material-ui/core/CardContent"
-import { makeStyles } from "@material-ui/core/styles"
+import GridList from "@material-ui/core/GridList"
+import GridListTile from "@material-ui/core/GridListTile"
+import GridListTileBar from "@material-ui/core/GridListTileBar"
+import Chip from "@material-ui/core/Chip"
+import AddIcon from "@material-ui/icons/AddCircle"
+import IconButton from "@material-ui/core/IconButton"
 
 import RecommendedLocations from "../components/RecommendedLocations"
 import { calcDistance, intersection, extractLngLat } from "../helpers/distance"
-import DistanceSlider from "../components/DistanceSlider"
-import ActivitiesSelector from "../components/ActivitiesSelector"
 import { Typography } from "@material-ui/core"
-import TextField from "@material-ui/core/TextField"
-
-const useStyles = makeStyles({
-    card: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: "inline-block",
-        margin: "0 2px",
-        transform: "scale(0.8)",
-    },
-    title: {
-        fontSize: 16,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-})
-const FilterPanel = props => {
-    const classes = useStyles()
-    const { setDistanceThreshold, setActivities, setSearchString } = props
-    const onSearchInputChange = event => {
-        if (event.target.value) {
-            setSearchString(event.target.value)
-        } else {
-            setSearchString("")
-        }
-    }
-
-    return (
-        <div>
-            <Grid container direction="column" spacing={4}>
-                <Grid item>
-                    <Card>
-                        <CardContent>
-                            <Typography className={classes.title} gutterBottom color="textSecondary">
-                                Filter by distance
-                            </Typography>
-                            <DistanceSlider
-                                defaultDistanceThreshold={100}
-                                handleSliderChange={(e, newValue) => {
-                                    setDistanceThreshold(newValue)
-                                }}
-                            />
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item>
-                    <Card style={{ overflow: "visible" }}>
-                        <CardContent>
-                            <Typography className={classes.title} gutterBottom color="textSecondary">
-                                Filter by activity
-                            </Typography>
-                            <ActivitiesSelector onChange={value => setActivities(value)} />
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item>
-                    <Card>
-                        <CardContent>
-                            <Typography className={classes.title} gutterBottom color="textSecondary">
-                                Filter by name
-                            </Typography>
-                            <TextField
-                                id="searchInput"
-                                placeholder="Filter Locations"
-                                margin="normal"
-                                onChange={onSearchInputChange}
-                            />
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+import { FilterPanel } from "../components/FilterPanel"
 
 function filterLocations(locationList, data, distanceThreshold, activities, searchString) {
     activities = activities ? activities : []
@@ -111,14 +37,17 @@ function filterLocations(locationList, data, distanceThreshold, activities, sear
 export default function DiscoverPanel(props) {
     const defaultDistanceThreshold = 300
     let { locationList, setChosen } = props
-    // For debugging only, should be imported
+
+    // For debugging only, should be imported from backend!
     const userLocation = { lat: 45.95, lng: -73.33 }
+
     let data = locationList.map(loc => ({
         name: loc.name,
         distance: calcDistance({ lat: loc.lat, lng: loc.long }, userLocation),
         activities: loc.activities,
         latitude: loc.lat,
         longitude: loc.long,
+        img: loc.img,
     }))
 
     const [activities, setActivities] = useState([])
@@ -136,14 +65,14 @@ export default function DiscoverPanel(props) {
     return (
         <div style={{ padding: 20 }}>
             <Grid container spacing={6}>
-                <Grid xs={9}>
+                {/* <Grid xs={9}>
                     <Typography variant="h6">
                         Here you can review locations other users have added, and choose to add them to your own Wildly
                         monitor. You can filter by activity, distance, and location name.
                     </Typography>
-                </Grid>
+                </Grid> */}
                 <Grid xs={9}>
-                    <MapView locationList={filteredLocations} />
+                    <MapView height="60vh" locationList={filteredLocations} />
                 </Grid>
                 <Grid xs={3}>
                     <Grid container direction="column" style={{ padding: 50 }}>
@@ -155,10 +84,38 @@ export default function DiscoverPanel(props) {
                             />
                         </Grid>
                         <Grid item xs={12} style={{ padding: 50 }} />
-                        <Grid item xs={12}>
+                        {/* <Grid item xs={12}>
                             <RecommendedLocations data={recommendations} setChosen={() => {}} style={{ padding: 50 }} />
-                        </Grid>
+                        </Grid> */}
                     </Grid>
+                </Grid>
+            </Grid>
+            <Grid container spacing={6}>
+                <Grid xs={9}>
+                    {recommendations.length > 0 ? (
+                        <GridList cols={6}>
+                            {recommendations.map(location => (
+                                <GridListTile>
+                                    <img src={location.img} alt={location.title} />
+                                    <GridListTileBar
+                                        title={location.name}
+                                        subtitle={location.activities.map(activity => (
+                                            <Chip label={activity} />
+                                        ))}
+                                        actionIcon={
+                                            <IconButton aria-label={`Add location`}>
+                                                <AddIcon color="secondary" />
+                                            </IconButton>
+                                        }
+                                    ></GridListTileBar>
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    ) : (
+                        <Typography color="textSecondary" align="center">
+                            Use the filters to the right to find locations to add to Wildly
+                        </Typography>
+                    )}
                 </Grid>
             </Grid>
         </div>
